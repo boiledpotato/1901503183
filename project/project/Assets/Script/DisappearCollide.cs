66a29ac3aefdf01dbd943fef30650c3cbf91d88c
boiledpotato <devinams@gmail.com>
2017-11-21 21:15:17 +0700

    project

---
+++ project/project/Assets/Script/DisappearCollide.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisappearCollide : MonoBehaviour {
+
+    public AudioSource audio;
+    public AudioClip clip;
+    public Text countText;
+
+    private int count;
+    
+
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            
+            other.gameObject.SetActive(false);
+            count = count + 100;
+            SetCountText(); 
+            audio.PlayOneShot(clip);
+            
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+    }
+}
---
+++ project/project/Assets/Script/GameOver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public float threshold;
+    // Use this for initialization
+    public Text gameoverText;
+    void Start()
+    {
+        gameoverText.text = "";
+    }
+    void FixedUpdate()
+    {
+        if (transform.position.y < threshold)
+            gameoverText.text = "Game Over";
+
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("FinishLine"))
+            gameoverText.text = "Stage Cleared!";
+        
+    }
+
+}
